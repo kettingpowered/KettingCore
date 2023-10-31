073077b69993513120cec50c8b9fd38e9094e424
JustRed23 <manuel.bormans@gmail.com>
2023-10-31 22:00:57 +0100

    Forge does not like reflections

--- src/main/java/org/kettingpowered/ketting/core/Ketting.java
+++ src/main/java/org/kettingpowered/ketting/core/Ketting.java
@@ -3,14 +3,13 @@ package org.kettingpowered.ketting.core;
 import org.jetbrains.annotations.NotNull;
 import org.kettingpowered.adapter.ForgeAdapter;
 import org.kettingpowered.ketting.core.injectprotect.InjectProtect;
-import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.kettingpowered.adapters.*;
+
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public final class Ketting {
 
@@ -19,7 +18,7 @@ public final class Ketting {
     private static final Ketting INSTANCE = new Ketting();
     private static String mcVersion;
 
-    private final Map<String, ForgeAdapter> AVAILABLE_ADAPTERS = new HashMap<>();
+    private final List<ForgeAdapter> AVAILABLE_ADAPTERS = new ArrayList<>();
 
     public static Ketting init(String mcVersion) {
         if (Ketting.mcVersion != null)
@@ -49,7 +48,9 @@ public final class Ketting {
     }
 
     public @NotNull ForgeAdapter getAdapter() {
-        ForgeAdapter adapter = AVAILABLE_ADAPTERS.get(mcVersion);
+        ForgeAdapter adapter = AVAILABLE_ADAPTERS.stream()
+                .filter(adptr -> adptr.getMcVersion().equals(mcVersion))
+                .findFirst().orElse(null);
 
         if (adapter == null)
             throw new RuntimeException("Could not find an adapter for Minecraft version " + mcVersion);
@@ -58,15 +59,6 @@ public final class Ketting {
     }
 
     private void collectAdapters() {
-        Reflections reflections = new Reflections("org.kettingpowered.adapters");
-
-        reflections.getSubTypesOf(ForgeAdapter.class).forEach(clazz -> {
-            try {
-                ForgeAdapter adapter = clazz.getDeclaredConstructor().newInstance();
-                AVAILABLE_ADAPTERS.put(adapter.getMcVersion(), adapter);
-            } catch (Exception e) {
-                LOGGER.error("Could not instantiate adapter " + clazz.getName(), e);
-            }
-        });
+        AVAILABLE_ADAPTERS.add(new Forge_1_20_2_Adapter());
     }
 }
