f4bbab81eadd1e3b20fc33551fccff5a18ccb589
JustRed23 <manuel.bormans@gmail.com>
2023-11-20 20:08:01 +0100

    Update dimension registry to use key and value

--- src/main/java/org/kettingpowered/ketting/adapter/DimensionRegistry.java
+++ src/main/java/org/kettingpowered/ketting/adapter/DimensionRegistry.java
@@ -1,8 +1,9 @@
 package org.kettingpowered.ketting.adapter;
 
-public interface DimensionRegistry<R> {
+public interface DimensionRegistry<R, K, V> {
 
     String getMcVersion();
     void createDefaults();
     void register(R registry);
+    K getKey(V value);
 }
