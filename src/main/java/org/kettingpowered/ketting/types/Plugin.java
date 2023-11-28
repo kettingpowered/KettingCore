c65153b6229828735457555cc297e586f0a6861a
JustRed23 <manuel.bormans@gmail.com>
2023-11-28 14:16:58 +0100

    Change plugin to use a supplier as being enabled can change

--- src/main/java/org/kettingpowered/ketting/types/Plugin.java
+++ src/main/java/org/kettingpowered/ketting/types/Plugin.java
@@ -1,11 +1,12 @@
 package org.kettingpowered.ketting.types;
 
 import java.io.File;
+import java.util.function.Supplier;
 import java.util.logging.Logger;
 
 public record Plugin(
         PluginInfo info,
-        boolean enabled,
+        Supplier<Boolean> enabled,
         File dataFolder,
         Logger logger
 ) {}
