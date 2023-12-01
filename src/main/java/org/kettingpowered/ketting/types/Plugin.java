50e0a4ce11befbf19a6392872ca29b6519ffc05a
JustRed23 <manuel.bormans@gmail.com>
2023-12-01 11:10:21 +0100

    Use BooleanSupplier instead

--- src/main/java/org/kettingpowered/ketting/types/Plugin.java
+++ src/main/java/org/kettingpowered/ketting/types/Plugin.java
@@ -1,12 +1,12 @@
 package org.kettingpowered.ketting.types;
 
 import java.io.File;
-import java.util.function.Supplier;
+import java.util.function.BooleanSupplier;
 import java.util.logging.Logger;
 
 public record Plugin(
         PluginInfo info,
-        Supplier<Boolean> enabled,
+        BooleanSupplier enabled,
         File dataFolder,
         Logger logger
 ) {}
