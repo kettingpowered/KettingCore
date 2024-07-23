ba362518dded1935452f4a057351791ab590a443
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2024-07-23 23:51:50 +0200

    Add FTBTeams to DispatcherRedirector

--- src/main/java/org/kettingpowered/ketting/common/utils/DispatcherRedirector.java
+++ src/main/java/org/kettingpowered/ketting/common/utils/DispatcherRedirector.java
@@ -3,11 +3,13 @@ package org.kettingpowered.ketting.common.utils;
 import java.util.Arrays;
 import java.util.Collections;
 
+@SuppressWarnings("unused")
 public class DispatcherRedirector {
 
     public static String[] REDIRECTED = {
             "org.popcraft.chunky.ChunkyForge",
-            "team.creative.playerrevive.PlayerRevive"
+            "team.creative.playerrevive.PlayerRevive",
+            "dev.ftb.mods.ftbteams.data.FTBTUtils"
     };
 
     public static boolean needsRedirect() {
